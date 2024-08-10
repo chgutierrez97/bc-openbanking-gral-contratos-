45893dd0f3819a2f9e6b67445f91c0141e9b5cbf
christian <christian.gutierrez@tepuit.com>
2024-08-09 22:36:26 -0400

    actualizacion

---
+++ src/main/java/com/ve/bc/openbanking/dto/ResponseServicio.java
@@ -0,0 +1,15 @@
+package com.ve.bc.openbanking.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ResponseServicio {
+	private String tracerId;
+	
+	private RespuestaConError errorConsulta;
+	
+	private ServicioResponse servicio;
+
+}
